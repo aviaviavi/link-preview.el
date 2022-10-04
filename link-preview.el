49b362bce9654da725d2237dcb171f38324ff824
aviaviavi <avipress@gmail.com>
2022-10-04 13:20:00 -0700

    General cleanup, packaging conventions

--- link-preview.el
+++ link-preview.el
@@ -3,7 +3,8 @@
 ;; Copyright (C) 2022 Avi Press
 ;; Author: Avi Press <mail@avi.press>
 ;; Version: 1.0
-;; URL: https://github.com/aviaviavi/link-preview-emacs
+;; URL: https://github.com/aviaviavi/link-preview.el
+;; Package-Requires: ((request "20210816.200"))
 
 ;;; Commentary:
 
@@ -14,11 +15,11 @@
 (require 'json)
 (require 'request)
 
-(setq link-preview-api "https://api.peekalink.io/")
-
-(defun lp/expand-link-at-cursor ()
+(defun link-preview-insert ()
+  "Injects a link preview for the URL under your cursor."
   (interactive)
-  (setq link-preview-url (thing-at-point 'url))
+  (let ((link-preview-api "https://api.peekalink.io/")
+        (link-preview-url (thing-at-point 'url)))
   (message "json: %S" (json-encode `(("link" . ,link-preview-url))))
   (request link-preview-api
     :type "POST"
@@ -36,12 +37,11 @@
                 (insert (assoc-default 'title data))
                 (newline-and-indent)
                 (insert (assoc-default 'description data))
-                (newline-and-indent)
-                ))
+                (newline-and-indent)))
     :complete (lambda (&rest _) (message "Finished!"))
     :error
     (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
-                  (message "Got error: %S" error-thrown)))))
+                  (message "Got error: %S" error-thrown))))))
 
-
-
+(provide 'link-preview)
+;;; link-preview.el ends here
