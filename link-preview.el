c686e0e2114a28c3b5b8b897d4f922b2b6281db7
aviaviavi <avipress@gmail.com>
2022-10-04 16:48:08 -0700

    Indentation

--- link-preview.el
+++ link-preview.el
@@ -20,28 +20,28 @@
   (interactive)
   (let ((link-preview-api "https://api.peekalink.io/")
         (link-preview-url (thing-at-point 'url)))
-  (message "json: %S" (json-encode `(("link" . ,link-preview-url))))
-  (request link-preview-api
-    :type "POST"
-    :sync t
-    :loglevel 'debug
-    :data (json-encode `(("link" . ,link-preview-url)))
-    :parser 'json-read
-    :headers `(("Content-Type" . "application/json") ("X-API-Key" . ,(getenv "PEEKALINK_API_KEY")))
-    :success (cl-function
-              (lambda (&key data &allow-other-keys)
-                (message "%S" (assoc-default 'title data))
-                (message "%S" (assoc-default 'description data))
-                (end-of-line)
-                (forward-line 1)
-                (insert (assoc-default 'title data))
-                (newline-and-indent)
-                (insert (assoc-default 'description data))
-                (newline-and-indent)))
-    :complete (lambda (&rest _) (message "Finished!"))
-    :error
-    (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
-                  (message "Got error: %S" error-thrown))))))
+    (message "json: %S" (json-encode `(("link" . ,link-preview-url))))
+    (request link-preview-api
+      :type "POST"
+      :sync t
+      :loglevel 'debug
+      :data (json-encode `(("link" . ,link-preview-url)))
+      :parser 'json-read
+      :headers `(("Content-Type" . "application/json") ("X-API-Key" . ,(getenv "PEEKALINK_API_KEY")))
+      :success (cl-function
+                (lambda (&key data &allow-other-keys)
+                  (message "%S" (assoc-default 'title data))
+                  (message "%S" (assoc-default 'description data))
+                  (end-of-line)
+                  (forward-line 1)
+                  (insert (assoc-default 'title data))
+                  (newline-and-indent)
+                  (insert (assoc-default 'description data))
+                  (newline-and-indent)))
+      :complete (lambda (&rest _) (message "Finished!"))
+      :error
+      (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
+                     (message "Got error: %S" error-thrown))))))
 
 (provide 'link-preview)
 ;;; link-preview.el ends here
